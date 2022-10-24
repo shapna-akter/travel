b4018ee20644563c1df467fd5d2d7819471d593b
Shapna <shapna088@gmail.com>
2022-10-24 17:07:26 +0600

    hide

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAt8OCwnkQyd0xfTkhJcq8CquQth-q33LM",
-  authDomain: "travel-guru-99d10.firebaseapp.com",
-  projectId: "travel-guru-99d10",
-  storageBucket: "travel-guru-99d10.appspot.com",
-  messagingSenderId: "602228496931",
-  appId: "1:602228496931:web:e6424f9488c1ed90e36f96"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
